03de3f40a1ebf58d1b29bb0e1e927b8c91d055b5
Marius Wallin <post@mariuswallin.no>
2025-09-03 11:39:59 +0200

    update shop

--- shop/components/BuyButton.tsx
+++ shop/components/BuyButton.tsx
@@ -1,9 +1,12 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, type GestureResponderEvent } from "react-native";
 import { productStyles } from "./styles";
 
 export default function BuyButton() {
+  const handlePress = (e: GestureResponderEvent) => {
+    console.log("Button pressed", e.currentTarget);
+  };
   return (
-    <Pressable onPress={() => {}} style={productStyles.button}>
+    <Pressable onPress={handlePress} style={productStyles.button}>
       <Text style={{ color: "white", fontWeight: "bold" }}>Kjøp</Text>
     </Pressable>
   );
