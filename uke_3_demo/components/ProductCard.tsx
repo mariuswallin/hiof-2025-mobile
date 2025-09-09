f0fa4b9b0ca884ec373b8ee7c33747c97d03b6e1
Marius Wallin <post@mariuswallin.no>
2025-09-09 13:59:39 +0200

    fix: add image

--- uke_3_demo/components/ProductCard.tsx
+++ uke_3_demo/components/ProductCard.tsx
@@ -3,12 +3,16 @@ import { productStyles } from "./styles";
 import ProductName from "./ProductName";
 import ProductPrice from "./ProductPrice";
 import BuyButton from "./BuyButton";
+import { Image } from "expo-image";
+
+import MyImage from "../assets/img.jpg";
 
 export default function ProductCard() {
   return (
     <View style={productStyles.card}>
       <ProductName />
       <ProductPrice />
+      <Image source={MyImage} style={{ width: 200, height: 200 }} />
       <BuyButton />
     </View>
   );
